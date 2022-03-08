1d9106190f65057f1d2926b035c57862e13888ee
Christian Morales <christianm9916@gmail.com>
2022-03-08 10:41:29 -0600

    Bootstrap Installed

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import Button from 'react-bootstrap/Button'
 
 function App() {
   return (
@@ -17,6 +18,9 @@ function App() {
         >
           Learn React
         </a>
+        <Button>
+          Hi
+        </Button>
       </header>
     </div>
   );
